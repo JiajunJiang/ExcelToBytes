4065c0f600b2249a3dbaa4678e80d8f58ab31030
James <465021744@qq.com>
2022-04-15 02:51:02 +0800

    Add global config

---
+++ global_config.py
@@ -0,0 +1,26 @@
+global package_name
+package_name = "Data"
+
+# java only
+global java_package
+java_package = "com.example.data"
+
+# java only
+global java_outer_classname
+java_outer_classname = "Data"
+
+# True/False
+global java_multiple_files
+java_multiple_files = True
+
+# SPEED = 0 / CODE_SIZE =1 / LITE_RUNTIME = 2
+global optimize_for
+optimize_for = 0
+
+# True/False
+global cc_enable_arenas
+cc_enable_arenas = True
+
+# Objective-C only
+global objc_class_prefix
+objc_class_prefix = "NS"
