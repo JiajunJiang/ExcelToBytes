96c233bfa243e6b2517838232d2221ef0235bd88
James <465021744@qq.com>
2022-04-16 02:51:23 +0800

    layout file header

--- interpreter.py
+++ interpreter.py
@@ -35,4 +35,15 @@ class interpreter:
 
     def layout_file_header(self):
         self.content.append("syntax = \"proto3\";\n")
-        self.content.append("package = %s \n" % global_config.package_name)
+        self.content.append("package = %s;\n" % global_config.package_name)
+        if self.language_type == global_config.language_type.java:
+            self.content.append("option java_package = %s;" % global_config.java_package)
+            self.content.append("option java_outer_classname = %s;" % global_config.java_outer_classname)
+            self.content.append(
+                "option java_outer_classname = %s;" % ("true" if global_config.java_multiple_files else "false"))
+            self.content.append("option optimize_for = %s;" % global_config.optimize_for)
+
+        if self.language_type == global_config.language_type.cplus:
+            self.content.append("option optimize_for = %s;" % global_config.optimize_for)
+            self.content.append(
+                "option cc_enable_arenas = %s;" % ("true" if global_config.cc_enable_arenas else "false"))
