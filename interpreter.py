e7faf1ca16edb22e9c9c73b1203afbbca1fb2d5c
James <465021744@qq.com>
2022-04-16 21:38:08 +0800

    check field type

--- interpreter.py
+++ interpreter.py
@@ -79,14 +79,41 @@ class interpreter:
         return key.__contains__(self.flag)
 
     def check_rule(self):
+        field_type = str(self.sheet.cell_value(FIELD_TYPE_ROW, self.current_col))
         rule = str(self.sheet.cell_value(FIELD_RULE_ROW, self.current_col))
         name = str(self.sheet.cell_value(FIELD_NAME_ROW, self.current_col))
         desc = str(self.sheet.cell_value(FIELD_DESC_ROW, self.current_col))
+
+        if not self.check_flag(field_type):
+            sys.exit("Error Field Type Col = {} name = {}".format(self.current_col, name))
+
         if rule == "optional":
-            self.content.append("optional {} = {}; //{}\n".format(name, self.field_count, desc))
+            self.content.append("optional {} {} = {}; //{}\n".format(field_type, name, self.field_count, desc))
             self.field_count += 1
         elif rule == "repeated":
-            self.content.append("repeated {} = {}; //{}\n".format(name, self.field_count, desc))
+            self.content.append("repeated {} {} = {}; //{}\n".format(field_type, name, self.field_count, desc))
             self.field_count += 1
         else:
             sys.exit("Field Rule Error rule = {} name = {}".format(rule, name))
+
+    def check_type(self, field_type):
+        if field_type == "int32" \
+                or field_type == "int64" \
+                or field_type == "uint32" \
+                or field_type == "uint64" \
+                or field_type == "sint32" \
+                or field_type == "sint64" \
+                or field_type == "fixed32" \
+                or field_type == "fixed64" \
+                or field_type == "sfixed32" \
+                or field_type == "sfixed64" \
+                or field_type == "float" \
+                or field_type == "double" \
+                or field_type == "bool" \
+                or field_type == "string":
+            return True
+        elif field_type.__contains__("map"):
+            # todo map field
+            return True
+        else:
+            return False
