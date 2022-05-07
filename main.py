d11b54c2abe01ef9596830845ceb0c90114c0e0d
James <465021744@qq.com>
2022-05-08 00:22:25 +0800

    support build client and sever together

--- main.py
+++ main.py
@@ -13,31 +13,10 @@ from parse import parse
 
 importlib.reload(sys)
 
+
 # argv 1 = Excel File Path
 # argv 2 = Flag                 c - Client s - Server
-if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        sys.exit("please use path flag operation")
-
-    excel_file = sys.argv[1]
-    flag = sys.argv[2]
-
-    try:
-        workbook = xlrd.open_workbook(excel_file)
-    except BaseException as e:
-        print("open ExcelFile(%s) failed!" % excel_file)
-        raise
-
-    language = ""
-    if flag == 'c':
-        language = global_config.client_language
-        print("Choose Client")
-    elif flag == 's':
-        language = global_config.server_language
-        print("Choose Server")
-    else:
-        sys.exit("params 2 flag is not c or s")
-
+def build(language, flag):
     for sheet in workbook.sheets():
         if "Sheet" in sheet.name or "#" in sheet.name:
             continue
@@ -50,3 +29,31 @@ if __name__ == '__main__':
         archive(language, flag)
 
         print("Build {} Success".format(sheet.name))
+    pass
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        sys.exit("please input excel_file")
+
+    excel_file = sys.argv[1]
+    if len(sys.argv) == 3:
+        flag = sys.argv[2]
+    else:
+        flag = 'cs'
+
+    try:
+        workbook = xlrd.open_workbook(excel_file)
+    except BaseException as e:
+        print("open ExcelFile(%s) failed!" % excel_file)
+        raise
+
+    language = ""
+    if flag.__contains__('c'):
+        print("Choose Client")
+        build(global_config.client_language, 'c')
+    if flag.__contains__('s'):
+        print("Choose Server")
+        build(global_config.server_language, 's')
+    if not flag.__contains__('c') and not flag.__contains__('s'):
+        sys.exit("params 2 flag is not c or s")
